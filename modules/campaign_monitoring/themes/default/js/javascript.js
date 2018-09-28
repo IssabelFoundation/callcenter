b2592ff0e3cdc6909d4392a3c4e6a38b70cb977c
HGMnetwork.com <30288264+hgmnetwork@users.noreply.github.com>
2018-09-28 14:33:00 +0100

    Update javascript.js
    añadir icono escucha en cada agente y ejecutar script para llamar a funcion que realice escucha

--- modules/campaign_monitoring/themes/default/js/javascript.js
+++ modules/campaign_monitoring/themes/default/js/javascript.js
@@ -535,3 +535,37 @@ function mostrar_mensaje_error(s)
 		}, 5000);
 	});
 }
+
+//hgmnetwork.com funciona para realizar escucha a los agentes 24-08-2018
+function escuchar(agente,extension){
+        //reemplazamos cualquier etiqueta dejando solo el texto del agente por ejemplo SIP/agente o A/agente Agent/agente SIP/5001 A/5001
+        const regex = /(<([^>]+)>)/ig;
+        var agente = agente.replace(regex, '');
+        //ahora quitamos el espacio inicial o cualquiera
+        var agente = agente.replace(' ', '');
+
+//        alert("realizamos llamada de chanspy con el agente: (" + agente+" a la extension "+ extension);
+
+        $.ajax({
+                  url: "/modules/campaign_monitoring/libs/escuchar_agente.php",
+                  type: "get", //send it through get method
+                          data: {
+			    agente: agente,
+                            extension: extension,
+                          },
+
+                          success: function(response) {
+                            //Do Something
+//                        alert(response);
+                            console.log(response);
+                          },
+                          error: function(xhr) {
+                            //Do Something to handle error
+                            console.log('error escuchar_agente.php');
+                            console.log(xhr);
+                         alert("Se ha producido un error al intentar escuchar.")
+                          }
+        });
+
+
+};
